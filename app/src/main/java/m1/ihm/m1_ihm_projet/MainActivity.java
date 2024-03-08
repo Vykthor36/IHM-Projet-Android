15fa0b03f16bde1f95ec042bb17ed3d3a7b22f45
Vykthor36 <victor.laurin.pro@gmail.com>
2024-03-08 12:13:35 +0100

    Initial commit

---
+++ app/src/main/java/m1/ihm/m1_ihm_projet/MainActivity.java
@@ -0,0 +1,14 @@
+package m1.ihm.m1_ihm_projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
